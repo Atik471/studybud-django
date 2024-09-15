5ce14991fa313b67457a3ae750a38fc4ffc9c603
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-09-15 08:30:50 +0600

    created database tables

--- base/views.py
+++ base/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': "Let's learn python"},
-    {'id': 2, 'name': "Let's learn Java"},
-    {'id': 3, 'name': "Let's learn Javascript"}, 
-]
+# rooms=[]
+
+# rooms = [
+#     {'id': 1, 'name': "Let's learn python"},
+#     {'id': 2, 'name': "Let's learn Java"},
+#     {'id': 3, 'name': "Let's learn Javascript"}, 
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
+    room = Room.objects.get(id=pk)
 
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
     
     context = {'room': room}
 
