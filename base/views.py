185a37d7fb1684df60e4bfb431bf528938e39e3c
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-09-15 06:59:11 +0600

    added templates

--- base/views.py
+++ base/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': "Let's learn python"},
+    {'id': 2, 'name': "Let's learn Java"},
+    {'id': 3, 'name': "Let's learn Javascript"}, 
+]
 
 def home(request):
-    return HttpResponse('This is home page')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('This is room page')
+def room(request, pk):
+    room = None
+
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
