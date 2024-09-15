2fa8e7b4599727e0a2d696bb53dcd1ec94a145b7
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-09-15 10:51:51 +0600

    added CRUD operation on rooms

--- base/models.py
+++ base/models.py
@@ -9,11 +9,15 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
+    host = models.ForeignKey(User,  on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     update = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-update', '-created']
 
     def __str__(self):
         return self.name
