6a11d5dbae52e74476ac949f79d8ac89e6579a56
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-09-25 14:29:43 +0600

    bug fixed for vercel

--- studybud/settings.py
+++ studybud/settings.py
@@ -135,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # for vercel
 import os
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
